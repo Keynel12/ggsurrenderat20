0bc87fc24c413e26ca46927974e1fdb18de3be49
andonnnn1 <andonnnn1@abv.bg>
2014-10-31 09:04:13 +0200

    Very nice
    The best ever

---
+++ age dilema.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int age = 35;
+ cin >> age;
+     if (age == 19) {
+         cout << "I am not old yet";}
+
+     else if  (age > 19){
+           cout << "I am too old for this";}
+     else if (age < 19){
+         cout << "I am waaaaay too you young!";}
+ return 0;
+}
